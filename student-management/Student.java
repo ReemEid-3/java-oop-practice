a0c775b64e2b1c4a61906e49cbe01a1c84396762
RE <Reem1232eid@hotmail.com>
2026-02-04 18:39:52 +0300

    Create Student.java

---
+++ student-management/Student.java
@@ -0,0 +1,18 @@
+public class Student {
+
+    private int id;
+    private String name;
+    private double gpa;
+
+    public Student(int id, String name, double gpa) {
+        this.id = id;
+        this.name = name;
+        this.gpa = gpa;
+    }
+
+    public void printInfo() {
+        System.out.println("ID: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("GPA: " + gpa);
+    }
+}
