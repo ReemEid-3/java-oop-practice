d7b8879a22cfb00c23fb1b9f005ccc1de75040ab
RE <Reem1232eid@hotmail.com>
2026-02-04 18:41:12 +0300

    Create Main.java

---
+++ student-management/student-management/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Student s1 = new Student(1, "Reem", 4.5);
+        Student s2 = new Student(2, "Sara", 4.8);
+
+        s1.printInfo();
+        System.out.println("------------");
+        s2.printInfo();
+    }
+}
